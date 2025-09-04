f2127eac8c6359d0eeefc001d81aaa7ab57e4b18
Austin Miller <amiller@mathworks.com>
2025-09-04 12:21:56 -0400

    removed sourcegraph & cody

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,7 +8,6 @@ return { -- override nvim-cmp plugin
     local cmp = require "cmp"
     -- modify the sources part of the options table
     opts.sources = cmp.config.sources {
-      { name = "cody", priority = 1000 },
       { name = "nvim_lsp", priority = 950 },
       { name = "luasnip", priority = 750 },
       { name = "buffer", priority = 500 },
